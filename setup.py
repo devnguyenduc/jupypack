010dca577bbba465fb4f46bdc0506e0b23e7e98c
test <>
2020-06-12 10:03:32 +0700

    fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
         name="jupypack",
         version="1.0.0",
-        packages=[jupypack.printpack],
+        packages=['jupypack.printpack'],
         package_dir={'':'jupypack'},
         url="https://github.com/devnguyenduc/jupypack.git",
         license="",
