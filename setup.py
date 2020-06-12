2dbde48c3f407a0828b667ed7f41182078a120bb
test <>
2020-06-12 10:06:26 +0700

    fix n

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
         name="jupypack",
         version="1.0.0",
-        packages=['jupypack.printpack'],
-        package_dir={'':''},
+        packages=['jupypack'],
+        package_dir={'':'jupypack'},
         url="https://github.com/devnguyenduc/jupypack.git",
         license="",
         author="",
