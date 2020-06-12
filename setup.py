28190a5d20c80cea083fec530fd9246418de0452
test <>
2020-06-12 09:55:41 +0700

    checkfile

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+        name="jupypack",
+        version="1.0.0",
+        packages=[jupypack.printpack],
+        package_dir={'':'jupypack'},
+        url="https://github.com/devnguyenduc/jupypack.git",
+        license="",
+        author="",
+        author_email="",
+        description="null"
+        )
