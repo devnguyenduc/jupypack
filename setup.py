70b13ca125eaf8ce597b76a0df631cb0ab495b9c
test <>
2020-06-12 10:09:12 +0700

    c

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
         name="jupypack",
         version="1.0.0",
         packages=['jupypack'],
-        package_dir={'':'jupypack'},
+        package_dir={'':''},
         url="https://github.com/devnguyenduc/jupypack.git",
         license="",
         author="",
